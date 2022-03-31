4a49dcd4b7a933b05aea2f6ed1ce72bbc47bab80
Yun Young Choi <87300199+olsi10@users.noreply.github.com>
2022-03-31 09:16:21 +0900

    string formatting - 문자열 포매팅

--- 01.py
+++ 01.py
@@ -96,38 +96,58 @@
 
 #ex)  정수 출력시
 
-num = 50
-s = 'my age %d' % num
+# num = 50
+# s = 'my age %d' % num
 
-print(s)
+# print(s)
 
-#문자열 %s
-# 정수 %d
-# 실수 %f
-# 8진수 %o
-# 16진수 %x
-# 문자표현 %%#
+# #문자열 %s
+# # 정수 %d
+# # 실수 %f
+# # 8진수 %o
+# # 16진수 %x
+# # 문자표현 %%#
 
-names = ['kim', 'park', 'lee']
-for name in names:
-    print('my name is %s' % name)
+# names = ['kim', 'park', 'lee']
+# for name in names:
+#     print('my name is %s' % name)
 
-money = 10000
-s2 = 'give me %d won' % money
-print(s2)
+# money = 10000
+# s2 = 'give me %d won' % money
+# print(s2)
 
-d = 3.141592
-print('value %f' % d)
+# d = 3.141592
+# print('value %f' % d)
 
-##########formatting 해야 하는 수가 두 개 이상일 때
+# ##########formatting 해야 하는 수가 두 개 이상일 때
 
-s1 = 'my name is %s age : %d' % ('mirim', 100)
-print(s1)
+# s1 = 'my name is %s age : %d' % ('mirim', 100)
+# print(s1)
 
-age = 78
-money = 20000
-name = 'Jang'
-weight = 63.12
-etc = 'asdf'
-s2 = 'my name is %s, age : %d, weight  : %f, money : %d, etc : %s' % (name, age, weight, money, etc)
-print(s2)
+# age = 78
+# money = 20000
+# name = 'Jang'
+# weight = 63.12
+# etc = 'asdf'
+# s2 = 'my name is %s, age : %d, weight  : %f, money : %d, etc : %s' % (name, age, weight, money, etc)
+# print(s2)
+
+# % 서식 문자를 이용한 문자열 포매팅은 언뜻 보면
+# 타입을 정해주기 때문에 정확해 보이지만, 타입을 정해야 하기 때문에
+# 불편함도 존해
+# 
+# 이를 개선하기 위해서 str.format 형식 등장
+# 그 이후에 f-string 방법 등장
+# 본인의 취향에 맞게, 편리한 방법 사용
+
+#string formatting - 문자열 포매팅
+#문자열에서 특정 부분만 바구고 나머지 부분은 일정하다고 할 때
+#문자열 포매팅을 이용해서 보기
+
+month = 1
+
+while month <= 12:
+     print(f'2020년 {month}월')
+     month = month + 1 #증감
+
+#변해야 하는 값이 있느 위치를 포매팅 할 위치를 잡아서 설정

60c5745355a71d123c84d784964f7e32d3b80b31
olsi10 <wangbampang2035@gmail.com>
2022-04-20 23:30:35 +0900

    Make strudent variable, Make our_team variable

---
+++ Python/과제.py
@@ -0,0 +1,17 @@
+student_grade = 2
+student_class = 3
+student_number = 11
+student_name = "최윤영"
+student_height = 160.3
+
+print(type(student_grade))
+print(type(student_class))
+print(type(student_number))
+print(type(student_name))
+print(type(student_height))
+
+our_team = ["김비야", "김유진", "박선주", "백선미", "안소영", "양혜원", "이혜령", "임재연", "최윤영", "최혜민", "하도연", "하진"]
+
+
+#10번째 학생이름을 출력해보자
+print(our_team[9])

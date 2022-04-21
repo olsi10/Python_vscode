9473c2e69f252f59d9571b0907bee9c8034dbf9f
Yun Young Choi <87300199+olsi10@users.noreply.github.com>
2022-04-21 10:09:29 +0900

    딕셔너리 복습

--- Python/ListTupleSetDict.py
+++ Python/ListTupleSetDict.py
@@ -377,25 +377,44 @@ nums = list(range(3))
 
 
 # 딕셔너리 생성
-d = {}
-print(d)
+# d = {}
+# print(d)
 
+# urls = {"google" : "google.com", 18 : "unesco.org"}
+# print(urls)
+
+# # 딕셔너리 요소 추가
+# urls["x"] = 2560 # 키 X가 딕셔너리에 없으면 추가
+# print(urls)
+
+# # 딕셔너리 요소 수정
+# urls["x"] = 1920
+# print(urls)
+
+# # 딕셔너리 요소 제거
+# del urls["x"]
+# urls.pop(18)
+
+# print(urls)
+
+# urls.clear()
+# print(urls)
+
+# 딕셔너리 요소 검색
 urls = {"google" : "google.com", 18 : "unesco.org"}
-print(urls)
 
-# 딕셔너리 요소 추가
-urls["x"] = 2560 # 키 X가 딕셔너리에 없으면 추가
-print(urls)
+print(urls["google"])
 
-# 딕셔너리 요소 수정
-urls["x"] = 1920
-print(urls)
+print(urls.get("google"))
 
-# 딕셔너리 요소 제거
-del urls["x"]
-urls.pop(18)
+print("google" in urls)
+print("google.com" in urls)
+print("google.com" in urls.values())
 
-print(urls)
+# 딕셔너리 관련 함수
+print(len(urls))
 
-urls.clear()
-print(urls)
+print(urls.keys())
+print(urls.values())
+print(urls.items())
+

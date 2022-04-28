5efa0f3b2965cec7415f4fa70d4363f2516c4f24
Yun Young Choi <87300199+olsi10@users.noreply.github.com>
2022-04-28 09:07:25 +0900

    얕은 복사 사용 방법 및 특징

--- Python/MutableImutable.py
+++ Python/MutableImutable.py
@@ -127,3 +127,53 @@
 # print(f"d1 값 : {d1} \t 주소 : {hex(id(d1))}")
 # print(f"d2 값 : {d2} \t 주소 : {hex(id(d2))}")
 # print(f"d3 값 : {d3} \t 주소 : {hex(id(d3))}")
+
+############################## 0428 ###############################
+
+#mutable 안에 immutable
+
+# 리스트, 딕셔너리 mutable
+
+# print("=" * 50)
+
+# arr1 = [55, 66, [11, 22], 'a', 'b']
+# arr2 = [55, 66, [11, 22], 'a', 'b']
+
+# print(f"arr1 : {arr1}\t 주소 : {hex(id(arr1))}")
+# print(f"arr2 : {arr2}\t 주소 : {hex(id(arr2))}")
+
+# print()
+
+# print(f"arr1[0] : {arr1[0]} \t주소 : {hex(id(arr1[0]))}")
+# print(f"arr2[0] : {arr2[0]} \t주소 : {hex(id(arr2[0]))}")
+# print(f"arr1[1] : {arr1[1]} \t주소 : {hex(id(arr1[1]))}")
+# print(f"arr2[1] : {arr2[1]} \t주소 : {hex(id(arr2[1]))}")
+# print(f"arr1[3] : {arr1[3]} \t주소 : {hex(id(arr1[3]))}")
+# print(f"arr2[3] : {arr2[3]} \t주소 : {hex(id(arr2[3]))}")
+# print(f"arr1[4] : {arr1[4]} \t주소 : {hex(id(arr1[4]))}")
+# print(f"arr2[4] : {arr2[4]} \t주소 : {hex(id(arr2[4]))}")
+
+# print()
+
+# print(f"arr1[2] : {arr1[2]} \t주소 : {hex(id(arr1[2]))}")
+# print(f"arr2[2] : {arr2[2]} \t주소 : {hex(id(arr2[2]))}")
+
+# 리스트 내부에 있는 mutable 요소를 보면 arr1[2], arr2[2]에 있는 리스트 [11, 22]는 각각 주소가 다름
+
+#################################################################################
+
+# 얕은 복사 (값이 아닌 주소 참조, 같은 곳을 가리킴)
+arr1 = [1,2,3]
+arr2 = arr1
+
+print(f"arr1 : {arr1} \t 주소 : {hex(id(arr1))}")
+print(f"arr2 : {arr2} \t 주소 : {hex(id(arr2))}")
+
+print()
+
+# 내용물에 4 추가
+arr1.append(4)
+
+# arr1과 arr2에 4 추가됨
+print(f"arr1 : {arr1} \t 주소 : {hex(id(arr1))}")
+print(f"arr2 : {arr2} \t 주소 : {hex(id(arr2))}")

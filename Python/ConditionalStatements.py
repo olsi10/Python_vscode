1e92e00795ad0aeae8baf891d17c85e76f35b976
Yun Young Choi <87300199+olsi10@users.noreply.github.com>
2022-05-19 10:07:53 +0900

    (변수) = 1 (True), 0 (False)

---
+++ Python/ConditionalStatements.py
@@ -0,0 +1,37 @@
+# a = 10
+
+# if a > 5:
+#     print("a가 10입니다.")
+#     print("test입니다.")
+# else:
+#     print("else문입니다.")
+
+# x = int(input()) # 형변환처럼 입력된 타입을 int로 바꿔줌
+
+# print(x)
+# print(type(x))
+
+# if x % 2 == 0:
+#     print("짝수입니다.")
+# else:
+#     print("홀수입니다.")
+
+# password = input("숫자 입력 : ")
+
+# if password == "암호":
+#     print("암호.")
+# else:
+#     print("암호 아님.")
+
+# money = 1 # True
+# if money:
+#     print("택시를")
+# print("타고")
+ #   print("가자")
+
+money = 2000
+card = 1 # True
+if money >= 3000 or card:
+     print("택시 타기")
+else:
+    print("걸어가기")

457959840567bbbc97d8fdc0260556553a0fae4e
Yun Young Choi <87300199+olsi10@users.noreply.github.com>
2022-05-19 10:15:21 +0900

    shift 연산자는 수행 속도를 높인다

---
+++ Python/TrueAndFalse.py
@@ -0,0 +1,19 @@
+# bool
+# 0 false / 1 tre -> 파이썬은 앞 문자가 대문자
+
+print(bin(0b1010))
+print(bin(0b1010 & 0b0110))
+print(bin(0b1010 | 0b0110))
+print(bin(0b1010 ^ 0b0110))
+print(bin(~0b1010))
+print(~0b1010)
+
+print(0b1010)
+print(0b1010 & 0b0110)
+print(0b1010 | 0b0110)
+print(0b1010 ^ 0b0110)
+
+print(bin(0b1010 << 2))
+print(0b1010 << 2)
+print(bin(0b1010 >> 2))
+print(0b1010 >> 2)

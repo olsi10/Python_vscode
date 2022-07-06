a539196c4aaccb22c23f0b79100ca75307f7b4cd
olsi10 <wangbampang2035@gmail.com>
2022-07-07 04:14:03 +0900

    테스트 파일

---
+++ 수업 내용/시험 정리/01.py
@@ -0,0 +1,14 @@
+print("{:d}".format(515))
+print("{:5d}".format(515))
+print("{:+5d}".format(515))
+print("{:05d}".format(515))
+print("{:=+5d}".format(515))
+print("{:+05d}".format(515))
+
+print()
+
+print("{:f}".format(1.17))
+print("{:12f}".format(1.1))
+print("{:12.1f}".format(1.17))
+
+print("{:5.2f}".format(1.114))

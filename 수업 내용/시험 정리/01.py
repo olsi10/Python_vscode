ae9799c9a8099473e349cf2f0e7919c45c1cb920
olsi10 <wangbampang2035@gmail.com>
2022-07-07 04:49:05 +0900

    리스트 마무리

--- 수업 내용/시험 정리/01.py
+++ 수업 내용/시험 정리/01.py
@@ -1,14 +1,36 @@
-print("{:d}".format(515))
-print("{:5d}".format(515))
-print("{:+5d}".format(515))
-print("{:05d}".format(515))
-print("{:=+5d}".format(515))
-print("{:+05d}".format(515))
+# print("{:d}".format(515))
+# print("{:5d}".format(515))
+# print("{:+5d}".format(515))
+# print("{:05d}".format(515))
+# print("{:=+5d}".format(515))
+# print("{:+05d}".format(515))
 
-print()
+# print()
 
-print("{:f}".format(1.17))
-print("{:12f}".format(1.1))
-print("{:12.1f}".format(1.17))
+# print("{:f}".format(1.17))
+# print("{:12f}".format(1.1))
+# print("{:12.1f}".format(1.17))
 
-print("{:5.2f}".format(1.114))
+# print("{:5.2f}".format(1.114))
+
+# num = []
+
+# num += [0, 1, 10, 22, 30]
+
+# nums = list(range(3))
+
+# print(num[2])  # 10
+
+# print(10 in num)
+
+# print(3 in num)
+
+# print(range(3))
+# print(range(0, 10))
+# print(range(1, 10, 2))
+
+# print(set(range(1, 10, 2)))
+# print(list(range(1, -5, -2)))
+
+# for i in range(3):
+#     print(i)

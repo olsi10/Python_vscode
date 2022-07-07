7e6b7879fd589e8cd0e1dbe8a4b88967b2b3c7ca
olsi10 <wangbampang2035@gmail.com>
2022-07-08 04:57:42 +0900

    논리 연산 / 비트 연산

--- 수업 내용/시험 정리/01.py
+++ 수업 내용/시험 정리/01.py
@@ -253,19 +253,19 @@
 
 # print(game)
 
-a = {1, 2, 3, 4}
-b = {3, 4, 5, 6}
+# a = {1, 2, 3, 4}
+# b = {3, 4, 5, 6}
 
-print(a & b)
-print(a.intersection(b))
+# print(a & b)
+# print(a.intersection(b))
 
-print(a | b)
-print(a.union(b))
+# print(a | b)
+# print(a.union(b))
 
-print(a - b)
-print(b - a)
+# print(a - b)
+# print(b - a)
 
-print(a.difference(b))
+# print(a.difference(b))
 
-print(a ^ b)
-print(a.symmetric_difference(b))
+# print(a ^ b)
+# print(a.symmetric_difference(b))

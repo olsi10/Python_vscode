bd25583b9a124a19a55f9e01b210fe46ccc2658e
olsi10 <wangbampang2035@gmail.com>
2022-07-13 20:15:50 +0900

    혼공단 2일차

---
+++ 혼공파/01.py
@@ -0,0 +1,28 @@
+# pi = 3.141592
+# print(pi)
+
+
+# pi = pi + 2
+# print(pi)
+
+# pi = pi * 2
+# print(pi)
+
+# pi = pi - 2
+# print(pi)
+
+# pi = pi / 2
+# print(pi)
+
+# pi = pi ** 2
+# print(pi)
+
+# 원에 둘레, 넓이 구하기
+
+pi = 3.141592
+r = 10  # 반지름
+
+print("원주율 : ", pi)
+print("반지름 : ", r)
+print("둘레 : ", 2 * pi * r)
+print("넓이 : ", pi * r * r)

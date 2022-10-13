dfc23ff508c1717a1d6de6813dab9b358136fc19
olsi10 <wangbampang2035@gmail.com>
2022-10-13 10:12:52 +0900

    객체 생성 메세지 출력

--- 수업 내용/클래스/carConstructor.py
+++ 수업 내용/클래스/carConstructor.py
@@ -2,9 +2,12 @@ class Car():
     def __init__(self, type, speed):
         self.type = type
         self.speed = speed
+        print(self.type, "객체 생성")
 
 
 c = Car('스포츠카', 200)
+c1 = Car('경차', 300)
+c2 = Car('리무진', 100)
 
 # print(c.type) # 스포츠카
 # print(c.speed) # 200
---
+++ 수업 내용/클래스/constructorMessage.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self):
+        print("객체 생성")
+
+
+p1 = Person()
+p2 = Person()
+p3 = Person()
+p4 = Person()

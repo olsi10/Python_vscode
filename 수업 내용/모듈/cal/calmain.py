aa194ae1d805c970026091eec7f4998e3f1ed040
olsi10 <wangbampang2035@gmail.com>
2022-10-13 09:20:57 +0900

    사칙연산 모듈

---
+++ 수업 내용/모듈/cal/calmain.py
@@ -0,0 +1,12 @@
+import calculator
+
+a = int(input("첫 번째 수는? "))
+b = int(input("두 번째 수는? "))
+
+print(a, " + ", b, " =", calculator.add(a, b))
+
+print(a, " - ", b, " =", calculator.sub(a, b))
+
+print(a, " * ", b, " =", calculator.mul(a, b))
+
+print(a, " / ", b, " =", calculator.div(a, b))

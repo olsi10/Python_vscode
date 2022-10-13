5c6e48cc567b74267e5e1aec5b30881abc71091a
olsi10 <wangbampang2035@gmail.com>
2022-10-13 09:55:08 +0900

    시험 문제 예상 : 4 line

---
+++ 수업 내용/클래스/pet.py
@@ -0,0 +1,26 @@
+class Pet:
+    # 짖다
+    # 어느 한 부분에서 특정적으로 실행되는 객체 = 인스턴스
+    # 반드시 첫 번째 매개변수가 주어저야 한다. 대부분 self 사용. 클래스 메서드는 반드시 self라는 첫 번ㅉㅐ 매개변수
+    def bark_dog():
+        print("왈왈~")
+    def bark_cat(self):
+        print("야용~")
+    def bark_mouse(self):
+        print("쮴쮴쮞")
+
+
+# java = Pet p1 = new Pet()
+# 파이 = p1 = Pet()
+
+p1 = Pet()
+p1.bark_dog()
+
+p2 = Pet()
+p2.bark_cat()
+p2.bark_dog()
+
+p3 = Pet()
+p3.bark_dog()
+p3.bark_cat()
+p3.bark_mouse()

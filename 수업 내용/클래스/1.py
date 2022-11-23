700bc6078337b2a2269a00602b601644297ae9fe
olsi10 <wangbampang2035@gmail.com>
2022-11-23 23:48:34 +0900

    클래스

---
+++ 수업 내용/클래스/1.py
@@ -0,0 +1,20 @@
+class Car:
+    count = 0
+
+    def __init__(self, type, speed):
+        self.type = type
+        self.speed = speed
+        Car.count += 1
+
+    # @classmethod
+    # def get_count(cls):
+    #     return cls.count
+
+    def get_count(self):
+        return Car.count
+
+
+print(Car.get_count())  # 안되는 이유는?
+c1 = Car("스포츠카", 100)
+c2 = Car("트럭", 50)
+print(Car.get_count())
